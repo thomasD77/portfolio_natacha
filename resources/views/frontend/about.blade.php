e7e319985fb9d370c34d7b4c8c108af1d769373f
thomasd77 <thomas.demeulenaere@hotmail.com>
2021-09-27 20:42:43 +0200

    pics

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <div class="row mt-4">
-                    <img class="img-fluid mt-5 w-50 mx-auto" src="{{asset('images/frontend/natacha_about.jpg')}}" alt="NatachaWaldmann">
+                    <img class="img-fluid mt-5 w-50 mx-auto" src="{{asset('images/frontend/about-natacha.jpg')}}" alt="NatachaWaldmann">
                     <h1 id="wie" class="my-5">Wie is Natacha Waldamnn...</h1>
                     <p>Sinds 2019 ben ik Schepen van Welzijn, Zorg en Onderwijs in Oostende, een portefeuille die me
                         nauw aan het hart ligt. Van opleiding ben ik namelijk orthopedagoge en oplossingsgericht
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 d-flex flex-column px-0">
-               <img class="img-fluid " src="{{asset('images/frontend/natacha.jpg')}}" alt="">
+               <img class="img-fluid " src="{{asset('images/frontend/home-natacha.jpg')}}" alt="">
             </div>
         </div>
     </div>
