6f879aea1e5649405b5ff630471cdd5bcb22f251
thomasD77 <thomas.demeulenaere@hotmail.com>
2021-10-17 13:29:56 +0200

    remarks

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends Controller
         $facebook = Facebook::latest()->paginate(2);
         $timeNow = Carbon::now()->toDateString();
         $posts = Post::with(['user', 'photo', 'postcategory'])->where('book', '<=', $timeNow)->latest()->limit(3)->get();
-        $footer_posts = Post::with(['user', 'photo', 'postcategory'])->where('book', '<=', $timeNow)->latest()->limit(4)->get();
+        $footer_posts = Post::with(['user', 'photo', 'postcategory'])->where('book', '<=', $timeNow)->latest()->limit(2)->get();
         return view('frontend.home', compact('posts', 'footer_posts', 'facebook'));
     }
 
